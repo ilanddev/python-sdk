3262c1b5f4581cb2510cdcd1950a23ebc5071ae2
Julien Anguenot <julien@anguenot.org>
2019-02-15 15:30:29 -0600

    Bump version: 1.0.1 → 1.0.2

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open('requirements_dev.txt') as requirements_dev_file:
 
 setup(
     name='iland-sdk',
-    version='1.0.1',
+    version='1.0.2',
     description="iland cloud Python SDK",
     long_description=readme + '\n\n' + history,
     author="iland Internet Solutions, Corp",
